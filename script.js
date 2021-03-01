f130a109f618a6192afcc48a85a50355e8939bc0
kodirupe <rupe.kodi@gmail.com>
2021-03-01 09:46:51 -0500

    Added container for jokes

---
+++ script.js
@@ -0,0 +1,11 @@
+//Object that contains arrays of the jokes
+const jokes = {
+    //One liner jokes
+    oneLine : [],
+
+    //Knock knock jokes
+    knockKnock : [],
+
+    //Random jokes
+    randomJoke : []
+};
